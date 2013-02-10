25a42d5ca4262f7043d7c97f2056e21a8991b612
Christian Lück <christian@lueck.tv>
2013-02-10 01:45:44 +0100

    Read asynchronously from inotify handler instead of polling
    Conflicts:
    
    	src/MKraemer/ReactInotify/Inotify.php

--- src/MKraemer/ReactInotify/Inotify.php
+++ src/MKraemer/ReactInotify/Inotify.php
@@ -13,32 +13,32 @@ class Inotify extends EventEmitter
     /**
      * @var resource
      */
-    protected $inotifyHandler;
+    protected $inotifyHandler = false;
 
     /**
      * @var array
      */
-    protected $watchDescriptors;
+    protected $watchDescriptors = array();
+    
+    /**
+     * @var React\EventLoop\LoopInterface
+     */
+    protected $loop;
 
     /**
-     * Constructor. Initializes the inotifyHandler and
-     * registers a periodicTimer in the eventLoop
+     * Constructor. Actual initialization takes place once first watched
+     * paths is registered during add()
      *
-     * @param React\EventLoop\LoopInterface $loop     Event Loop
-     * @param float                         $interval Interval in which new events should be read
+     * @param React\EventLoop\LoopInterface $loop Event Loop
+     * @see self::add()
      */
-    public function __construct(LoopInterface $loop, $interval = 0.1)
+    public function __construct(LoopInterface $loop)
     {
-        $this->inotifyHandler = \inotify_init();
-        stream_set_blocking($this->inotifyHandler, 0);
-
-        $this->watchDescriptors = array();
-
-        $loop->addPeriodicTimer($interval, $this);
+        $this->loop = $loop;
     }
 
     /**
-     * Checks if new inotify events are available
+     * Checks all new inotify events available
      * and emits them via evenement
      */
     public function __invoke()
@@ -59,7 +59,30 @@ class Inotify extends EventEmitter
      */
     public function add($path, $mask)
     {
+        if ($this->inotifyHandler === false) {
+            // initialize inotify handler
+            $this->inotifyHandler = \inotify_init();
+            stream_set_blocking($this->inotifyHandler, 0);
+            
+            // wait for any file events by reading from inotify handler asynchronously
+            $this->loop->addReadStream($this->inotifyHandler, $this);
+        }
         $descriptor = \inotify_add_watch($this->inotifyHandler, $path, $mask);
         $this->watchDescriptors[$descriptor] = array('path' => $path);
+    }
+
+    /**
+     * Clear all pending watched paths and close the inotifyHandler
+     */
+    public function close()
+    {
+        if ($this->inotifyHandler !== false) {
+            $this->loop->removeReadStream($this->inotifyHandler);
+            
+            fclose($this->inotifyHandler);
+            $this->inotifyHandler = false;
+            
+            $this->watchDescriptors = array();
+        }
     }
 }
